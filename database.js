143ea6c2a7065200c4a87f6d371313dccf98ecf6
Michael Marriott <mmarriott_sa@hotmail.com>
2020-08-30 23:45:22 +0200

    smappe on device

--- database.js
+++ database.js
@@ -1,43 +1,37 @@
 var {Pool} = require('pg');
 var {database_config} = require('./config.js');
+var log = require('./log.js');
 
 const pool = new Pool(database_config)
 let DefaultParentLocationId = 574
 
 const getDevicesByDeviceType = async(deviceTypeId) => {
-    //  const client = pool.connect();
       var results = [];
       try {
         const { rows } = await pool.query('SELECT * FROM device WHERE deviceTypeId = $1 ORDER BY device_id ASC',[deviceTypeId]);
-        // Stream results back one row at a time
-        //console.log(JSON.stringify(rows));
         return rows;
       }catch(err){
-          console.error('Database ' + err)
+          log.error('Database ' + err)
       }
-  
-      // After all data is returned, close connection and return results
-    //  await pool.release();
       return results;
 }
 exports.getDevicesByDeviceType = getDevicesByDeviceType;
  
 const getDevicesInfoByDeviceType = async(deviceTypeId) => {
-    //  const client = pool.connect();
       var results = [];
-      var sql = 'SELECT d.device_id, d.tele_period, d.identifier, l.identifier as location_identifier,l.username as location_username,l.password as location_password, '+
-      'e2.time as energy_time ,COALESCE(e2.total,\'0\') as energy_total ,COALESCE(e2.yesterday,\'0\') as energy_yesterday ,COALESCE(e2.today,\'0\') as energy_today '+
-      'FROM public.device d '+
-      'inner join public.location l on l.location_id = d.location_id '+
-      'left join (  select e1.device_id,e1.time,e1.total,e1.yesterday,e1.today from energy e1 inner join  '+
-        ' (select max(time) as time ,ei.device_id from energy ei group by ei.device_id) e on e.time = e1.time and e.device_id = e1.device_id)  e2 on e2.device_id = d.device_id '+
-      'WHERE d.devicetype_id = $1 and d.device_id = 720 '+
-      'ORDER BY d.device_id ASC';
+      var sql = `SELECT d.device_id, d.tele_period, d.identifier, l.identifier as location_identifier,l.username as location_username,l.password as location_password, 
+e2.time as energy_time ,COALESCE(e2.total,\'0\') as energy_total ,COALESCE(e2.yesterday,\'0\') as energy_yesterday ,COALESCE(e2.today,\'0\') as energy_today 
+FROM public.device d 
+inner join public.location l on l.location_id = d.location_id 
+left join (  select e1.device_id,e1.time,e1.total,e1.yesterday,e1.today from energy e1 inner join  
+  (select max(time) as time ,ei.device_id from energy ei group by ei.device_id) e on e.time = e1.time and e.device_id = e1.device_id)  e2 on e2.device_id = d.device_id 
+WHERE d.devicetype_id = $1 and l.is_active = true 
+      ORDER BY d.device_id ASC`;
       try {
         const { rows } = await pool.query(sql,[deviceTypeId]);
         return rows;
       }catch(err){
-          console.error('Database ' + err)
+          log.error('Database ' + err)
       }
   
   return results;
@@ -47,10 +41,9 @@ exports.getDevicesInfoByDeviceType = getDevicesInfoByDeviceType;
 const getDevice = async(deviceId) => {
       try {
         const { rows } = await pool.query('SELECT * FROM device WHERE device_id =$1 ORDER BY device_id ASC',[deviceId]);
-        //console.log(JSON.stringify(rows[0]));
         return rows[0];
       }catch(err){
-          console.error('Database ' + err)
+          log.error('Database ' + err)
       }
       return null;
 }
@@ -59,10 +52,9 @@ exports.getDevice = getDevice;
 const getDeviceByIdentifier = async(identifier) => {
     try {
       const { rows } = await pool.query('SELECT * FROM device WHERE identifier =$1 ORDER BY device_id ASC',[identifier]);
-      //console.log(JSON.stringify(rows[0]));
       return rows[0];
     }catch(err){
-        console.error('Database ' + err)
+        log.error('Database ' + err)
     }
     return null;
 }
@@ -70,10 +62,9 @@ exports.getDeviceByIdentifier = getDeviceByIdentifier;
 const getEnergy = async(deviceId) => {
     try {
         const { rows } = await pool.query('SELECT * FROM energy where device_id = $1 ORDER BY time DESC LIMIT 1', [deviceId]);
-        //console.log(JSON.stringify(rows));
         return rows;
     }catch(err){
-        console.error('Database ' + err)
+        log.error('Database ' + err)
     }
     return [];
 }
@@ -83,7 +74,7 @@ exports.getEnergy = getEnergy;
     await pool.query('INSERT INTO energy (time, device_id, total, yesterday,today, power,factor, voltage, current,utcdate,hourlyfactor) values ($1, $2, $3, $4,$5,$6,$7,$8,$9,$10,$11)  ',
       [time, device_id, total, yesterday,today, power,0, 0, 0,utcdate,hourlyfactor],(err,res)=> {
       if(err){
-          console.error(device_id+ ": Database " + err + " "+  time) 
+          log.error(device_id+ ": Database " + err + " "+  time) 
       }
   });
 }
@@ -93,7 +84,7 @@ const insertEnergy = (energy, device, time) => {
       pool.query('INSERT INTO energy (time, device_id, total, yesterday,today, power,factor, voltage, current) values($1, $2, $3, $4,$5,$6,$7,$8,$9)',
         [time, device.device_id, energy.Total, energy.Yesterday,energy.Today, energy.Power,energy.Factor, energy.Voltage, energy.Current],(err,res)=> {
         if(err){
-            console.error('Database ' + err) 
+            log.error('Database ' + err) 
         }
     });
 }
@@ -102,21 +93,20 @@ exports.insertEnergy = insertEnergy;
 const upsertTimer = ( device, number, timer) => {
     pool.query("SELECT count(*) FROM timer WHERE device_id = $1 and number = $2", [device.device_id, number],(err,res)=> {
         if(err){
-            console.error('Database ' + err) 
+            log.error('Database ' + err) 
         }else{
-            //console.log(res);
             if(res > 0){
                 pool.query('UPDATE timer SET (arm = $1, "time" = $2, "window" = 3$, days = 4$ , "repeat" = 5$, "output" = 6$, action = 7$) WHERE device_id = $8 and number = $9',
                 [ timer.arm,timer.Time, timer.window, timer.days, timer.repeat, timer.output, timer.action, device.device_id, number],(err,res)=> {
                  if(err){
-                    console.error('Database ' + err) 
+                    log.error('Database ' + err) 
                   }
                 });
             }else{
                 pool.query('INSERT INTO timer (device_id, numbered, arm, "time", "window", days, "repeat", "output", action) values($1, $2, $3, $4,$5,$6,$7,$8,$9)',
                 [ device.device_id, number, timer.arm,timer.time, timer.window, timer.days, timer.repeat, timer.output, timer.action],(err,res)=> {
                  if(err){
-                    console.error('Database ' + err) 
+                    log.error('Database ' + err) 
                   }
                 });
             }
@@ -130,13 +120,11 @@ exports.upsertTimer = upsertTimer;
 const getLocationByIdentifier = async(identifier) => {
   try {
     const { rows } = await pool.query('SELECT * FROM location WHERE identifier = $1',[identifier]);
-    //console.log(identifier,rows);
     if(rows.length > 0){
-      //console.log("found");
       return rows[0];
     }
   }catch(err){
-      console.error('Database ' + err)
+      log.error('Database ' + err)
   }
   return null;
 }
@@ -150,33 +138,33 @@ const insertSmappeeLocationAndDevice = async (identifier, name,username,password
   
   var location_id = await insertLocation({"Name":name,"TimezoneId":timezone_id,"CustomerId":customer_id,"Identifier":identifier, "ParentLocationId":DefaultParentLocationId,"IsActive": false});
   var device = { 
-    "Identifier": "Default", "Name": name, "Description": "Entire Store", "CustomerId":customer_id,"DeviceTypeId": devicetype_id, 
-  "LocationId":location_id, "TelePeriod": tele_period,"Username":username,"Password":password}
+    "Identifier": identifier, "Name": name, "Description": "Entire Store", "CustomerId":customer_id,"DeviceTypeId": devicetype_id, 
+  "LocationId":location_id, "TelePeriod": tele_period,"Username":username,"Password":password }
   await insertDevice(device);
   return location_id;
 };
 exports.insertSmappeeLocationAndDevice = insertSmappeeLocationAndDevice;
 
 const insertLocation = async(data)=>{
-  console.log('insertLocaton '); 
+  log.info('insertLocaton '); 
   let result = await pool.query(`INSERT INTO location (name, timezone_id, customer_id,identifier,parent_location_id,is_active) 
   values ($1,$2,$3,$4,$5,$6) RETURNING location_id`,
   [ data.Name, data.TimezoneId,data.CustomerId,data.Identifier,data.ParentLocationId, data.IsActive]);
   var location_id =  result.rows[0].location_id;
-  console.log('result ' + location_id);
+  log.info('result ' + location_id);
   return location_id;
 }
 exports.insertLocation = insertLocation;
 
 const insertDevice = async(device)=>{
-  console.error(JSON.stringify(device)); 
+  log.error(JSON.stringify(device)); 
   await pool.query(`INSERT INTO device 
     (identifier, name, description, customer_id, devicetype_id, location_id,tele_period, username, password, parent_device_id )
     values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) `,
     [ device.Identifier, device.Name, device.Description, device.CustomerId, device.DeviceTypeId, 
       device.LocationId, device.TelePeriod, device.Username,device.Password, null],(err,res)=> {
     if(err){
-        console.error('Database insertDevice: ' + err) 
+        log.error('Database insertDevice: ' + err) 
     }
   });
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var database = require('./database.js');
 const request = require('request');
-var {app_config} = require('./config.js');
+var log = require('./log.js');
+var { app_config } = require('./config.js');
 
 var AUTH_TOKEN = 'Bearer ';
 var API_URL = app_config.apiUrl;
+let dailyDays = 128;
+let hourlyDays = 10;
 
 async function addEnergy(datetime, deviceIdentifier, currentValue, yesterdayValue, totalValue, power, hourlyfactor) {
 	await database.insertSmappeeEnergy(totalValue, yesterdayValue, currentValue, power, deviceIdentifier, datetime, 2, hourlyfactor);
@@ -22,44 +25,43 @@ async function getData(device) {
 		var measureDayCurrentDateTime = new Date();
 		var measureHourlyCurrentDateTime = new Date();
 		measureHourlyCurrentDateTime.setHours(0);
-		measureDayCurrentDateTime.setDate(measureDayCurrentDateTime.getDate() - 88);
-		measureHourlyCurrentDateTime.setDate(measureHourlyCurrentDateTime.getDate() - 10);
-		console.log('getDataReading ' + device)	
-		console.log('lastDateTime ' + lastDateTime.getTime())	
+		measureDayCurrentDateTime.setDate(measureDayCurrentDateTime.getDate() - dailyDays);
+		measureHourlyCurrentDateTime.setDate(measureHourlyCurrentDateTime.getDate() - hourlyDays);
+		log.info('getDataReading ' + device)
+		log.info('lastDateTime ' + lastDateTime.getTime())
 		if (futureDateTime(lastDateTime, 1, 0, 0) < measureDayCurrentDateTime.getTime()) {
-			getDataReading(3, device.device_id, device.location_identifier, lastDateTime.getTime(), measureDayCurrentDateTime.getTime(), device.energy_total, device.energy_yesterday, device.energy_today, true);
+			getDataReading(3, device.device_id, device.identifier, lastDateTime.getTime(), measureDayCurrentDateTime.getTime(), device.energy_total, device.energy_yesterday, device.energy_today, true);
 		} else if (futureDateTime(lastDateTime, 0, 1, 0) < measureHourlyCurrentDateTime.getTime()) {
-			getDataReading(2, device.device_id, device.location_identifier, lastDateTime.getTime(), measureHourlyCurrentDateTime.getTime(), device.energy_total, device.energy_yesterday, device.energy_today, true);
+			getDataReading(2, device.device_id, device.identifier, lastDateTime.getTime(), measureHourlyCurrentDateTime.getTime(), device.energy_total, device.energy_yesterday, device.energy_today, true);
 		} else {
-			getDataReading(1, device.device_id, device.location_identifier, lastDateTime.getTime(), getCurrentDateTimeUTC, device.energy_total, device.energy_yesterday, device.energy_today, false);
+			getDataReading(1, device.device_id, device.identifier, lastDateTime.getTime(), getCurrentDateTimeUTC, device.energy_total, device.energy_yesterday, device.energy_today, false);
 		}
 	} else {
 		var lastDateTime = new Date();
 		lastDateTime.setDate(lastDateTime.getDate() - (365 * 5));
 		var measureMonthCurrentDateTime = new Date();
 		measureMonthCurrentDateTime.setDate(measureMonthCurrentDateTime.getDate() - 365)
-		getDataReading(4, device.device_id, device.location_identifier, lastDateTime.getTime(),
+		getDataReading(4, device.device_id, device.identifier, lastDateTime.getTime(),
 			measureMonthCurrentDateTime.getTime(), device.energy_total, device.energy_yesterday, device.energy_today, true);
 	}
 }
 
 //https://app1pub.smappee.net/dev/v2/servicelocation/39918/consumption?aggregation=3&from=1514768400000&to=1548624766770
 
-function getDataReading(aggregation, deviceId, locationIdentifier, fromTime, toTime, total, yesterday, today, forceSave) {
+function getDataReading(aggregation, deviceId, identifier, fromTime, toTime, total, yesterday, today, forceSave) {
 
-	if (locationIdentifier == null) {
+	if (identifier == null) {
 		return;
 	}
 
-	var url = API_URL + "servicelocation/" + locationIdentifier + "/consumption?aggregation=" + aggregation + "&from=" + fromTime + "&to=" + toTime + "";
+	var url = API_URL + "servicelocation/" + identifier + "/consumption?aggregation=" + aggregation + "&from=" + fromTime + "&to=" + toTime + "";
 	var currentValue = today;
 	var yesterdayValue = yesterday;
 	var totalValue = total;
 	var lastdate = fromTime;
 	var aggregation_closure = aggregation;
 
-
-		console.log(deviceId + ': url ' + url)
+	log.info(deviceId + ': url ' + url)
 	const options = {
 		url: url,
 		headers: {
@@ -68,7 +70,7 @@ function getDataReading(aggregation, deviceId, locationIdentifier, fromTime, toT
 	};
 
 	async function callback(error, response, body) {
-		//console.log(deviceId +": callback");
+		//log.info(deviceId +": callback");
 		var hourlyfactor = 1;
 		if (aggregation_closure == 1) {
 			hourlyfactor = 0.16666667;
@@ -83,27 +85,31 @@ function getDataReading(aggregation, deviceId, locationIdentifier, fromTime, toT
 			var len = info.consumptions.length;
 			var i = 0;
 			if (len == 0) {
+				if(aggregation_closure == 4){
+					lastdate = 	measureDayCurrentDateTime.setDate(measureDayCurrentDateTime.getDate() - dailyDays);
+				}
 				await addEnergy(new Date(lastdate), deviceId, 0, 0, 0, 0);
+				return;
 			}
 			var previousDatetimeStamp = lastdate;
 			for (var data of info.consumptions) {
 				i++;
 				if (i == len && !forceSave) {
-						console.log(deviceId + ": dont add the last one...");
+					log.info(deviceId + ": dont add the last one...");
 					break;
 				} else {
-						console.log(deviceId + ": " + i + "-" + len);
+					log.info(deviceId + ": " + i + "-" + len);
 				}
 
 				currentDateTime = new Date(data.timestamp);
 				if (currentDateTime <= new Date(lastdate)) {
-							console.log(deviceId + ":currentDateTime" + currentDateTime + "lastdate" + new Date(lastdate));
-							console.log(deviceId + ":already added.....................................");
+					log.info(deviceId + ":currentDateTime" + currentDateTime + "lastdate" + new Date(lastdate));
+					log.info(deviceId + ":already added.....................................");
 					continue;
 				}
 
 				if (nextday(previousDatetimeStamp, data.timestamp)) {
-							console.log(deviceId + ":new day" + previousDatetimeStamp + "-" + data.timestamp);
+					log.info(deviceId + ":new day" + previousDatetimeStamp + "-" + data.timestamp);
 					yesterdayValue = currentValue;
 					currentValue = 0;
 				}
@@ -114,17 +120,18 @@ function getDataReading(aggregation, deviceId, locationIdentifier, fromTime, toT
 				if (aggregation_closure == 4) {
 					hourlyfactor = 24 * daysInMonth(currentDateTime.getFullYear(), currentDateTime.getMonth());
 				}
-					console.log(deviceId + ":hourlyfactor: " + hourlyfactor);
+				log.info(deviceId + ":hourlyfactor: " + hourlyfactor);
 				await addEnergy(currentDateTime, deviceId, currentValue, yesterdayValue, totalValue, power, hourlyfactor);
 				previousDatetimeStamp = data.timestamp;
 			}
-				console.log(deviceId + " and we out...");
+			log.info(deviceId + " and we out...");
 
 		} else {
-			console.error('Error: ' + error + JSON.stringify(response));
+			log.error('Error: ' + error + JSON.stringify(response));
 		}
-		console.log("getDataReading callback done");
+		log.info("getDataReading callback done");
 	}
+
 	request(options, callback);
 }
 
@@ -148,12 +155,12 @@ function nextday(dateFrom, dateTo) {
 
 function daysInMonth(year, month) {
 	var x = new Date(year, month + 1, 0).getDate();
-	//console.log("daysInMonth" + x);
+	//log.info("daysInMonth" + x);
 	return x;
 }
 
 function addNewServiceLocation() {
-	console.log("addNewServiceLocation");
+	log.info("addNewServiceLocation");
 	var url = API_URL + "servicelocation/";
 	const options = {
 		url: url,
@@ -165,19 +172,19 @@ function addNewServiceLocation() {
 	async function callbackInsertSmappeeLocationAndDevice(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			const info = JSON.parse(body);
-			console.log('----------', info);
+			log.info('----------', info);
 			for (var data of info.serviceLocations) {
 				var location = await database.getLocationByIdentifier(data.serviceLocationId);
 				if (location == null) {
-					console.log("insertSmappeeLocationAndDevice");
+					log.info("insertSmappeeLocationAndDevice");
 					await database.insertSmappeeLocationAndDevice(data.serviceLocationId, data.name, null, null);
 				}
 			}
-			console.log("and we out...");
+			log.info("and we out...");
 		} else {
-			console.error('Error' + error + response);
+			log.error('Error' + error + response);
 		}
-		console.log("insertSmappeeLocationAndDevice callback done");
+		log.info("insertSmappeeLocationAndDevice callback done");
 		return;
 	}
 
@@ -199,13 +206,13 @@ function sleep(milliseconds) {
 		currentDate = Date.now();
 	} while (currentDate - date < milliseconds);
 }
-function setAuth(){
+function setAuth() {
 
 	const options = {
 		url: 'https://app1pub.smappee.net/dev/v2/oauth2/token',
 		method: 'POST',
 		headers: {},
-		form:{ 
+		form: {
 			'grant_type': 'password',
 			'client_id': 'CodeworxAPI',
 			'client_secret': 'FUuQqn52Yi',
@@ -218,35 +225,31 @@ function setAuth(){
 
 }
 
-function setAuthVariables(error, response, body){
-	console.log('----------', error);
-	const info = JSON.parse(body);
+function setAuthVariables(error, response, body) {
 	
+	const info = JSON.parse(body);
+
 	if (!error && response.statusCode == 200) {
 		const info = JSON.parse(body);
-		console.log('----------', info);
-		AUTH_TOKEN = 'Bearer ' +info.access_token
+		log.info('----------', info);
+		AUTH_TOKEN = 'Bearer ' + info.access_token
 
 		let x = (Math.random() * 10).toFixed(0)
-		if(x == 9){
+		if (x == 9) {
 			addNewServiceLocation();
 		}
 		database.getDevicesInfoByDeviceType(2).then(devices => {
-			console.log('-----///???-');
 			for (var device of devices) {
-				console.log(device)
+				log.info(device)
 				getData(device);
 			}
 		});
-
+	}else{
+		log.error('----------', error);
 	}
-
 }
 
+setAuth();
+setTimeout(() => { log.info("end of sleep!"); }, 40000);
 
-while (true){
-	setAuth();
-	setTimeout(() => { console.log("end of sleep!"); }, 40000);
-}
-
-console.log('finished at  ' + new Date());
+log.info('finished at  ' + new Date());
